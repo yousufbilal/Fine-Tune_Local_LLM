fda76452a5bf60852ad37f2f7fa5e049d063541a
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2026-05-31 17:04:17 +0100

    added training set

--- fine_tune.py
+++ fine_tune.py
@@ -3,7 +3,10 @@ import transformers
 import datasets
 import trl
 import json
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from datasets import Dataset
+from trl import SFTTrainer
+
 
 #im am fine tuniing a instruct model not a base model, I just need to make sure the data is in the right format for the instruct model
 model_name = "HuggingFaceTB/SmolLM2-135M-Instruct"
@@ -20,14 +23,26 @@ for item in file:
     formatted_item = format_prompt(item)
     formatted_data.append(formatted_item)
 
-
-
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # downloading the model
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-print("Model loaded!")
+dataset = Dataset.from_dict({"text": formatted_data})
 
+trainer = SFTTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    train_dataset=dataset,
+    dataset_text_field="text",
+    max_seq_length=512,
+    args=TrainingArguments(
+        output_dir="outputs",
+        num_train_epochs=3,
+        per_device_train_batch_size=2,
+        logging_steps=10,
+        report_to="none",
+    ),
+)
 
-
+trainer.train()
