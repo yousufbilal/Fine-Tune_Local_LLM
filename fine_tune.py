5dfd70c7c9c1365af8332be4266ad765ad7d4e87
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2026-05-31 13:38:46 +0100

    downloaded the instruct model

--- fine_tune.py
+++ fine_tune.py
@@ -3,6 +3,10 @@ import transformers
 import datasets
 import trl
 import json
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+#im am fine tuniing a instruct model not a base model, I just need to make sure the data is in the right format for the instruct model
+model_name = "HuggingFaceTB/SmolLM2-135M-Instruct"
 
 file = json.load(open("caveman_dataset.json", "r"))
 
@@ -16,8 +20,14 @@ for item in file:
     formatted_item = format_prompt(item)
     formatted_data.append(formatted_item)
 
-    # formatted_item = format_prompt(item)
-    # formatted_data.append(formatted_item)
 
-# print(formatted_data[0])
-print(formatted_item)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# downloading the model
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+print("Model loaded!")
+
+
+
